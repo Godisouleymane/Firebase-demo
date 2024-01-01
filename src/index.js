db5d23a0aacd034497ac39ae1c6af8b2ef044789
Godisouleymane <ssabiougodi@gmail.com>
2024-01-01 12:16:03 +0100

    realTime Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { addDoc, collection, getDocs, getFirestore, serverTimestamp, setDoc, doc } from 'firebase/firestore'
+import { addDoc, collection, getDocs, getFirestore, serverTimestamp, setDoc, doc, onSnapshot } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBkRg4J8MfDU6albnKxcsiuo0Jfw7oDiMw",
@@ -21,12 +21,16 @@ const utilisateurs = collection(dataBase, 'utilisateurs');
 
 const citiesRef = collection(dataBase, 'Villes');
 
-getDocs(utilisateurs).then((snapshot) => {
-    let utilisateurs = [];
+getDocs(citiesRef).then((snapshot) => {
+   // recuperer les donnees
+})
+
+onSnapshot(citiesRef, (snapshot) => {
+     let villes = [];
     snapshot.docs.forEach((doc) => {
-        utilisateurs.push({ ...doc.data(), id: doc.id })
+        villes.push({ ...doc.data(), id: doc.id })
     });
-    console.log(utilisateurs);
+    console.log(villes);
 })
 
 const addCityForm = document.querySelector(".ajouter");
@@ -34,18 +38,19 @@ addCityForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     // ajouter un document avec un id generer
-    // addDoc(citiesRef, {
-    //     pays: addCityForm.pays.value,
-    //     ville: addCityForm.ville.value,
-    //     capital: addCityForm.capital.value === 'true' ? true : false,
-    //     dateDajout: serverTimestamp()
-    // }).then(() => addCityForm.reset());
-
-    // ajouter un doc avec un id personaliser
-    setDoc(doc(dataBase, "Villes", "NIG"), {
+    addDoc(citiesRef, {
         pays: addCityForm.pays.value,
         ville: addCityForm.ville.value,
         capital: addCityForm.capital.value === 'true' ? true : false,
         dateDajout: serverTimestamp()
     }).then(() => addCityForm.reset());
-})
+
+    // ajouter un doc avec un id personaliser
+    // setDoc(doc(dataBase, "Villes", "NIG"), {
+    //     pays: addCityForm.pays.value,
+    //     ville: addCityForm.ville.value,
+    //     capital: addCityForm.capital.value === 'true' ? true : false,
+    //     dateDajout: serverTimestamp()
+    // }).then(() => addCityForm.reset());
+});
+
